480df061ef00a5c30d6d256791257bdc1b98beae
Adrián Cancio <ujiktl@gmail.com>
2024-09-13 15:17:17 +0200

    Commit inicial: Añadido primer proyectos de VS

---
+++ P01-Introduccion/Aleatorio/Aleatorio/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+using System.Security.Cryptography;
+using Apoyo;
+
+
+namespace Aleatorio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Ayuda ayuda = new Ayuda();
+            const int BYTES = 64;
+            byte[] bufer = new byte[BYTES];
+            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+            provider.GetBytes(bufer);
+            provider.Dispose();
+            
+            ayuda.WriteHex(bufer, BYTES);
+
+            const String NOMBRE_FICHERO = "NumeroAleatorio.bin";
+
+            ayuda.GuardaBufer(NOMBRE_FICHERO, bufer);
+            Console.WriteLine("Fichero de "+ ayuda.BytesFichero(NOMBRE_FICHERO) + " bytes guardado");
+
+            byte[] newBufer = new byte[BYTES];
+            ayuda.CargaBufer(NOMBRE_FICHERO, newBufer);
+
+            ayuda.WriteHex(newBufer, BYTES);
+
+        }
+    }
+}
