efb8139231110f59784aac2aaa54cdc4a1496d76
Adrián Cancio Vázquez <UO270394@uniovi.es>
2024-09-24 15:54:10 +0200

    Añadido avance practica AES

---
+++ P03-AES/CifradoAesCore/CifradoAesCore/Program.cs
@@ -0,0 +1,97 @@
+﻿using Apoyo;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CifradoAESCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int TamClave = 16;
+            byte[] Clave = new byte[TamClave];
+            for (int i = 0; i < Clave.Length; i++) Clave[i] = (byte)(i % 256);
+
+            byte[] VI = new byte[16];
+            for (int i = 0; i < VI.Length; i++) VI[i] = (byte)((i + 160) % 256);
+
+            byte[] TextoPlano =
+            { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37,
+             0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F,
+             0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37,
+             0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F,
+             0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37,
+             0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F
+            };
+
+            AesManaged Provider = new AesManaged();
+
+            Console.WriteLine("Provider default properties:");
+            Console.WriteLine("BlockSize: " + Provider.BlockSize);
+            Console.WriteLine("KeySize: " + Provider.KeySize);
+            Console.WriteLine("Padding: " + Provider.Padding);
+            Console.WriteLine("Mode: " + Provider.Mode);
+            Console.WriteLine();
+
+            Provider.Key = Clave;
+            Provider.Padding = PaddingMode.ISO10126;
+            Provider.Mode = CipherMode.ECB;
+
+            Console.WriteLine("Provider modified properties:");
+            Console.WriteLine("BlockSize: " + Provider.BlockSize);
+            Console.WriteLine("KeySize: " + Provider.KeySize);
+            Console.WriteLine("Padding: " + Provider.Padding);
+            Console.WriteLine("Mode: " + Provider.Mode);
+            Console.WriteLine();
+
+            Provider.GenerateKey();
+            
+            Ayuda ayuda = new Ayuda();
+            Console.WriteLine("Clave de cifrado:");
+            ayuda.WriteHex(Provider.Key, TamClave);
+
+            Provider.GenerateIV();
+            Console.WriteLine("Vector de inicialización:");
+            ayuda.WriteHex(Provider.IV, 16);
+
+            const String NombreFichero = "zz_TextoCifrado.bin";
+            FileStream fs = new FileStream(NombreFichero, 
+                FileMode.Create, FileAccess.Write, FileShare.None);
+
+            ICryptoTransform encryptor = Provider.CreateEncryptor();
+            
+            CryptoStream cs = new CryptoStream(fs, encryptor, CryptoStreamMode.Write);
+
+            cs.Write(TextoPlano, 0, TextoPlano.Length);
+            
+            cs.Flush();
+            cs.Close();
+            cs.Dispose();
+            fs.Close();
+
+            long TamFich = ayuda.BytesFichero(NombreFichero);
+            byte[] TextoDescifrado = new byte[TamFich];
+            fs = new FileStream(NombreFichero,
+                FileMode.Open, FileAccess.Read, FileShare.None);
+
+            ICryptoTransform decryptor = Provider.CreateDecryptor();
+
+            cs = new CryptoStream(fs, decryptor, CryptoStreamMode.Read);
+
+            cs.Read(TextoDescifrado, 0, TextoDescifrado.Length);
+            cs.Flush();
+            cs.Close();
+            fs.Close();
+
+            Console.WriteLine("\nTexto descifrado: ");
+            ayuda.WriteHex(TextoDescifrado, TextoDescifrado.Length);
+
+        }
+    }
+}
---
+++ P03-AES/CifradoStringAES/CifradoStringAES/Program.cs
@@ -0,0 +1,117 @@
+﻿using Apoyo;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CifradoStringAES
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int TamClave = 32;
+            byte[] Clave = new byte[TamClave];
+            for (int i = 0; i < Clave.Length; i++) Clave[i] = (byte)(i % 256);
+
+            byte[] VI = new byte[16];
+            for (int i = 0; i < VI.Length; i++) VI[i] = (byte)((i + 160) % 256);
+
+            byte[] TextoPlano =
+            { 
+                0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F,
+                0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F,
+                0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F
+            };
+
+            AesManaged Provider = new AesManaged();
+
+            Console.WriteLine("Provider default properties:");
+            Console.WriteLine("BlockSize: " + Provider.BlockSize);
+            Console.WriteLine("KeySize: " + Provider.KeySize);
+            Console.WriteLine("Padding: " + Provider.Padding);
+            Console.WriteLine("Mode: " + Provider.Mode);
+            Console.WriteLine();
+
+            Provider.Key = Clave;
+            Provider.Padding = PaddingMode.PKCS7;
+            Provider.Mode = CipherMode.ECB;
+
+            Console.WriteLine("Provider modified properties:");
+            Console.WriteLine("BlockSize: " + Provider.BlockSize);
+            Console.WriteLine("KeySize: " + Provider.KeySize);
+            Console.WriteLine("Padding: " + Provider.Padding);
+            Console.WriteLine("Mode: " + Provider.Mode);
+            Console.WriteLine();
+
+            Provider.GenerateKey();
+            
+            Ayuda ayuda = new Ayuda();
+            Console.WriteLine("Clave de cifrado:");
+            ayuda.WriteHex(Provider.Key, TamClave);
+
+            Provider.GenerateIV();
+            Console.WriteLine("Vector de inicialización:");
+            ayuda.WriteHex(Provider.IV, 16);
+
+            const String NombreFichero = "zz_TextoCifrado.bin";
+            FileStream fs = new FileStream(NombreFichero, 
+                FileMode.Create, FileAccess.Write, FileShare.None);
+
+            ICryptoTransform encryptor = Provider.CreateEncryptor();
+            
+            CryptoStream cs = new CryptoStream(fs, encryptor, CryptoStreamMode.Write);
+
+            StreamWriter sw = new StreamWriter(cs);
+
+            sw.WriteLine("Texto plano.");
+
+            sw.Flush(); 
+            sw.Close();
+            sw.Dispose();
+            cs.Flush();
+            cs.Close();
+            fs.Close();
+            cs.Dispose();
+
+            // Mostrar texto cifrado
+            long TamFich = ayuda.BytesFichero(NombreFichero);
+            byte[] TextoCifrado = new byte[TamFich];
+            for (int i = 0; i < TamFich; i++)
+            {
+                TextoCifrado[i] = 0xFF;
+            }
+            ayuda.CargaBufer(NombreFichero, TextoCifrado);
+
+            Console.WriteLine("\nTexto cifrado:");
+            ayuda.WriteHex(TextoCifrado, TextoCifrado.Length);
+
+
+            byte[] TextoDescifrado = new byte[TamFich];
+            fs = new FileStream(NombreFichero,
+                FileMode.Open, FileAccess.Read, FileShare.None);
+
+            ICryptoTransform decryptor = Provider.CreateDecryptor();
+
+            cs = new CryptoStream(fs, decryptor, CryptoStreamMode.Read);
+
+            StreamReader sr = new StreamReader(cs);
+
+            String TextoDescifradoPlano = sr.ReadToEnd();
+
+            cs.Flush();
+            cs.Close();
+            fs.Close();
+            sr.Close();
+            sr.Dispose();
+
+            Console.WriteLine("\nTexto descifrado: ");
+            Console.WriteLine(TextoDescifradoPlano);
+
+        }
+    }
+}
