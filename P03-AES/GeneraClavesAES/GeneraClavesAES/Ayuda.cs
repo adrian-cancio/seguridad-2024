3060b61efe10623a20f532d94065e957f40f8ea8
Adrián Cancio <ujiktl@gmail.com>
2024-09-26 19:18:40 +0200

    Finalizado GeneraClavesAES

---
+++ P03-AES/GeneraClavesAES/GeneraClavesAES/Ayuda.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// Para trabajar con ficheros
+using System.IO;
+
+
+namespace Apoyo
+{
+    class Ayuda
+    {
+        public void WriteHex(byte[] Bufer, int NumBytes)
+        {
+            int NumCols = 16;
+
+            // Console.WriteLine("NumBytes: " + NumBytes); // DEBUG
+            int NumFils = (int)Math.Ceiling((double)NumBytes / (double)NumCols);
+            // Console.WriteLine("NumFils: " + NumFils); // DEBUG
+
+            int i = 0;
+
+            for (int Fil = 0; Fil < NumFils; Fil++)
+            {
+                for (int Col = 0; Col < NumCols; Col++)
+                {
+                    Console.Write("{0,2:X2} ", Bufer[i]);
+                    i++;
+                    if (i >= NumBytes) break;
+                }
+                Console.WriteLine();
+            }
+        } // WriteHex()
+
+        public long BytesFichero(string NombreFich)
+        {
+            //return File.ReadAllBytes(NombreFich).Length;
+            FileStream fs = new FileStream(NombreFich, FileMode.Open);
+            /* Metodo byte a byte:
+            int bytes = 0;
+            while (fs.ReadByte() != -1)
+            {
+                bytes++;
+            }
+            fs.Close();
+            return bytes;
+            */
+            long bytes = fs.Length;
+            fs.Close();
+            return bytes;
+
+        } // BytesFichero()
+
+
+        public void GuardaBufer(string NombreFich, byte[] Bufer)
+        {
+            FileStream fs = new FileStream(NombreFich, FileMode.Create, FileAccess.Write, FileShare.None);
+            /* Metodo byte a byte
+            for (int i = 0; i < Bufer.Length; i++) 
+            {
+                fs.WriteByte(Bufer[i]);
+            }
+            */
+            fs.Write(Bufer, 0, Bufer.Length);
+            fs.Close();
+        } // GuardaBufer()
+
+
+        public void CargaBufer(string NombreFich, byte[] Bufer)
+        {
+            FileStream fs = new FileStream(NombreFich, FileMode.Open, FileAccess.Read, FileShare.None);
+            if (Bufer.Length < fs.Length)
+            {
+                Console.WriteLine("Tamaño de bufer insuficiente");
+                Environment.Exit(0);
+            }
+            fs.Read(Bufer, 0, Bufer.Length);
+            fs.Close();
+        } // CargaBufer()
+        
+    } // class Ayuda
+} // namespace Apoyo
---
+++ P03-AES/GeneraClavesAES/GeneraClavesAES/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Apoyo;
+
+namespace GeneraClavesAES
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce la clave: ");
+            String Contra = Console.ReadLine();
+
+            byte[] Sal = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+
+            Rfc2898DeriveBytes DeriveBytes = new Rfc2898DeriveBytes(Contra, Sal, 1000);
+
+            byte AESByteNum = 32; // 16 bytes para 128 bits, 24 bytes para 192 bits y 32 bytes para 256 bits
+            byte[] ClaveAES = DeriveBytes.GetBytes(AESByteNum);
+            byte[] VIAES = DeriveBytes.GetBytes(16);
+            
+            Ayuda ayuda = new Ayuda();
+
+            Console.WriteLine("Clave AES: ");
+            ayuda.WriteHex(ClaveAES, AESByteNum);
+            Console.WriteLine("Vector de Inicialización AES: ");
+            ayuda.WriteHex(VIAES, 16);
+            Console.WriteLine();
+
+            Console.WriteLine("Reseteamos el objeto DeriveBytes");
+            DeriveBytes.Reset();
+
+            byte[] NuevaClaveAES = DeriveBytes.GetBytes(AESByteNum);
+            byte[] NuevaVIAES = DeriveBytes.GetBytes(16);
+
+            Console.WriteLine("Nueva Clave AES: ");
+            ayuda.WriteHex(NuevaClaveAES, AESByteNum);
+            Console.WriteLine("Nuevo Vector de Inicialización AES: ");
+            ayuda.WriteHex(NuevaVIAES, 16);
+            Console.WriteLine();
+
+            Console.WriteLine("Pero si lo hacemos si reseterar:");
+            byte[] ClaveAES2 = DeriveBytes.GetBytes(AESByteNum);
+            byte[] VIAES2 = DeriveBytes.GetBytes(16);
+            Console.WriteLine("Clave AES: ");
+            ayuda.WriteHex(ClaveAES2, AESByteNum);
+            Console.WriteLine("Vector de Inicialización AES: ");
+            ayuda.WriteHex(VIAES2, 16);
+
+
+        }
+    }
+}
