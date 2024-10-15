262c022244bbe162bcc3e253d3809db74248c236
Adrián Cancio Vázquez <UO270394@uniovi.es>
2024-10-15 15:00:32 +0200

    Avance Practica 5B

--- P05B-AlmacenCertificados/BuscarCertificados/BuscarCertificados/Program.cs
+++ P05B-AlmacenCertificados/BuscarCertificados/BuscarCertificados/Program.cs
@@ -11,11 +11,24 @@ namespace BuscarCertificados
     {
         static void Main(string[] args)
         {
-            ExtraeCertificado("");
+            string nombreSujetoCer = "CN=zpUSU.as";
+            X509Certificate2 cert = ExtraeCertificado(nombreSujetoCer);
+            if (cert != null)
+            {
+                Console.WriteLine("Certificado encontrado");
+                Console.WriteLine();
+                Console.WriteLine("Sujeto: {0}", cert.SubjectName);
+                Console.WriteLine("Emisor: {0}", cert.Issuer);
+                Console.WriteLine("Número de serie: {0}", cert.SerialNumber);
+                Console.WriteLine("Válido desde: {0}", cert.NotBefore);
+                Console.WriteLine("Válido hasta: {0}", cert.NotAfter);
+                Console.WriteLine("Huella digital: {0}", cert.Thumbprint);
+            }
         }
 
         static X509Certificate2 ExtraeCertificado(String nombreCert)
         {
+            X509Certificate2 certBuscado = null;
             X509Store almacen = new X509Store(StoreName.My, StoreLocation.CurrentUser);
 
             //Console.WriteLine(almacen.Name);
@@ -24,11 +37,35 @@ namespace BuscarCertificados
             almacen.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
             X509Certificate2Collection coleCert = almacen.Certificates;
             almacen.Close();
+
+            /*
             foreach (X509Certificate cert in coleCert)
             {
                 Console.WriteLine(cert.Subject);
             }
-            return null;
+            */
+
+            Console.WriteLine("Numero de certificados: {0}", coleCert.Count);
+
+            //X509Certificate2Collection certsEncontrados = coleCert.Find(X509FindType.FindByTimeValid, DateTime.Now, false);
+            //X509Certificate2Collection certsEncontrados = coleCert.Find(X509FindType.FindBySubjectName, nombreCert, false);
+
+            X509Certificate2Collection certsEncontrados = coleCert.Find(X509FindType.FindBySubjectDistinguishedName, nombreCert, false);
+            switch (certsEncontrados.Count)
+            {
+                case 1:
+                    certBuscado = certsEncontrados[0];
+                    break;
+                case 0:
+                    Console.WriteLine("No se han encontrado certificados");
+                    break;
+                default:
+                    Console.WriteLine("Se ha encontrado mas de un certificado");
+                    break;
+            }
+
+
+            return certBuscado;
         }
     }
 }
